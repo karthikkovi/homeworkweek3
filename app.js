8edb963e26e7eb37917493a5a06c377c09d71369
Karthik Kovi <Karthi.kovi@gmail.com>
2020-03-01 22:51:27 +1030

    Added the password length column back.

--- app.js
+++ app.js
@@ -5,81 +5,62 @@ uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
 numbersForPassword = "0123456789";
 specialCharacters = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+passwordLength = 0;
 characterArray = "";
-
-//Get password length
-
-passwordLength = parseInt(prompt("Please enter the length of the password"));
-
-while (passwordLength <8){
-    passwordLength = parseInt(prompt("Please enter a value above 8 to get a secure password."));
-}
-
-useUpperCaseLetters = confirm("Do you want to use Uppercase Letters in the password?");
-useLowerCaseLetters = confirm("Do you want to use Lowercase letters in the password?");
-useNumbers = confirm("Do you want to use Numbers in the password?");
-useSpecialCharacters = confirm("Do you want to use special characters in the password?");
-
-if (useUpperCaseLetters) {
-    characterArray += uppercaseLetters;
-}
-if (useLowerCaseLetters) {
-    characterArray += lowercaseLetters;
-}
-if (useNumbers) {
-    characterArray += numbersForPassword;
-}
-if (useSpecialCharacters) {
-    characterArray += specialCharacters
-}
-// Determine what the user wants
-
-// inputUpperCase = document.getElementById("inputGroupUpperCase");
-// useUpperCaseLetters = inputUpperCase.options[inputUpperCase.selectedIndex].value;
-
-// inputLowerCase = document.getElementById("inputGroupLowerCase");
-// useLowerCaseLetters = inputLowerCase.options[inputLowerCase.selectedIndex].value;
-
-// inputNumbers = document.getElementById("inputGroupNumbers");
-// useNumbers = inputNumbers.options[inputNumbers.selectedIndex].value;
-
-// inputSpecialCharacters = document.getElementById("inputGroupSpecialCharacters");
-// useSpecialCharacters = inputSpecialCharacters.options[inputSpecialCharacters.selectedIndex].value;
-
-
-    // if (useUpperCaseLetters === "1"){
-    //     characterArray += uppercaseLetters;
-    // }
-    // if (useLowerCaseLetters === 1){
-        // characterArray += lowercaseLetters;
-    // }
-    // if (useNumbers === 1){
-        // characterArray += numbersForPassword;
-    // }
-    // if (useSpecialCharacters === 1) {
-        // characterArray += specialCharacters;
-    // }
-
-    console.log(characterArray);
-// Generate Password Function
-
-function generatePassword() {
-    let multiplier = characterArray.length;
-    let returnValue = "";
-    for (var i = 0; i < passwordLength; i++) {
-        returnValue += characterArray.charAt(Math.floor(Math.random() * multiplier));
-    }
-
-    return returnValue;
-}
 
 // Generate Password Button
 
 let generatedPassword = document.getElementById("generatePassword").addEventListener("click", () => {
+    getPasswordLength();
+    generateCharacterArray();
+    console.log(passwordLength);
+    console.log(characterArray);
+
     let multiplier = characterArray.length;
     let generatedPassword = "";
     for (var i = 0; i < passwordLength; i++) {
         generatedPassword += characterArray.charAt(Math.floor(Math.random() * multiplier));
     }
+    characterArray = "";
+    passwordLength = 0;
     document.getElementById("displayGeneratedPassword").innerText = generatedPassword;
 });
+
+function getPasswordLength() {
+    passwordLength = parseInt(document.getElementById("passwordLength").value);
+    return passwordLength;
+}
+
+//Generate character array 
+
+function generateCharacterArray() {
+
+    // Determine what the user wants
+
+    inputUpperCase = document.getElementById("inputGroupUpperCase");
+    useUpperCaseLetters = inputUpperCase.options[inputUpperCase.selectedIndex].value;
+
+    inputLowerCase = document.getElementById("inputGroupLowerCase");
+    useLowerCaseLetters = inputLowerCase.options[inputLowerCase.selectedIndex].value;
+
+    inputNumbers = document.getElementById("inputGroupNumbers");
+    useNumbers = inputNumbers.options[inputNumbers.selectedIndex].value;
+
+    inputSpecialCharacters = document.getElementById("inputGroupSpecialCharacters");
+    useSpecialCharacters = inputSpecialCharacters.options[inputSpecialCharacters.selectedIndex].value;
+
+
+    if (useUpperCaseLetters === "1") {
+        characterArray += uppercaseLetters;
+    }
+    if (useLowerCaseLetters === "1") {
+        characterArray += lowercaseLetters;
+    }
+    if (useNumbers === "1") {
+        characterArray += numbersForPassword;
+    }
+    if (useSpecialCharacters === "1") {
+        characterArray += specialCharacters;
+    }
+    return characterArray;
+}
