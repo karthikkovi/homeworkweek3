93c8481d967fd0d15b8e4087692f9a8a209b2d0e
Karthik Kovi <Karthi.kovi@gmail.com>
2020-03-01 19:58:25 +1030

    Created html css and js files. Added initial HTML and colors. Created variables in JavaScript

---
+++ app.js
@@ -0,0 +1,8 @@
+let password_length, uppercase_letters, lowercase_letters, numbers_for_password, special_characters;
+
+
+uppercase_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+lowercase_letters = "abcdefghijklmnopqrstuvwxyz";
+numbers_for_password = "0123456789";
+special_characters = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+
