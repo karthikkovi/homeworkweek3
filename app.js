0a96efba57b1da40d03090033629db3f43aa3c52
Karthik Kovi <Karthi.kovi@gmail.com>
2020-03-01 20:29:33 +1030

    replaced password lenght field with a prompt message

--- app.js
+++ app.js
@@ -6,3 +6,21 @@ lowercase_letters = "abcdefghijklmnopqrstuvwxyz";
 numbers_for_password = "0123456789";
 special_characters = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
 
+password_length = parseInt(prompt("Please enter the length of the password"));
+
+while (password_length <8){
+    password_length = parseInt(prompt("Please enter a value above 8 to get a secure password."));
+}
+
+console.log(password_length);
+
+// Generate Password Button
+
+// document.getElementById("generatePassword").addEventListener("click", ()=>{
+//     if (password_length != null) {
+//         password_length = parseInt(document.getElementById("passwordLength").value);
+//     } else {
+//         alert("Please enter the number of characters required for the password.")
+//     }
+// })
+
